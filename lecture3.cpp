5b3e0a8619a9971031717c462fda8459b3ea5faa
stasyblack <73378404+stasyblack@users.noreply.github.com>
2021-09-21 19:47:55 +0300

    Lecture3-variant 3 with changes. Class inheritance

--- lecture3.cpp
+++ lecture3.cpp
@@ -1,4 +1,23 @@
-﻿#include <iostream>
+﻿/*Задание лекция 3 - Software.
+Написать программу, которая:
+- умеет читать тектосвый файл, который может быть указан ей в качестве аргумента коммандной строки и содержит названия протоколов (одно на строке)
+Пример файла: 
+BinaryProtocol 
+HexProtocol 
+HexProtocol
+- Реализует классы Protocol, BinaryProtocol, HexProtocol
+- Классы BinaryProtocol, HexProtocol являются наследниками Protocol
+- Класс Protocol определяет виртуальный метод void send(char* buf, int len), который ничего не делает
+- Классы BinaryProtocol, HexProtocol переопределяют виртуальный метод void send(char* buf, int len)
+  - void BinaryProtocol::send(char* buf, int len) - печатает на экран содержимое строки buf в виде бинарного кода (т.е. при помощи "0101 0101 0100 1000")
+  - void HexProtocol::send(char* buf, int len)    - печатает на экран содержимое строки buf в виде шестнатеричного кода (т.е. так "0xff 0xff")
+Вариант 3.
+- Создать динамический массив указателей (размер равен числу протоколов в файле) на класс Animal и функцию 
+для добавления туда классов, соответствующих протоколам, считанным из файла
+- Написать функцию для печати отправки введенного с клавиатуры сообщения при помощи всех протоколов void sendAll(Protocol *p, int number_of_prorocols)
+*/
+
+#include <iostream>
 #include <Windows.h>
 #include <fstream>
 #include <bitset>
@@ -13,9 +32,16 @@ class Protocol
 public:
     
     virtual void send(char* buf, int len);
+};
+
+class ProtocolList
+{
+public:
+    Protocol** a;
     int readData();
-    int addProtocol(char* ndata, int len, int i, Protocol** a);
-    int sendAll(Protocol** a, int num);
+    int readFromFile(fstream& file, int i);
+    int addProtocol(char* ndata, int len, int i);
+    int sendAll(int num);
     int errorFileOpen(fstream& file);
     int errorInputData(fstream& file);
 };
@@ -32,7 +58,7 @@ public:
     virtual void send(char* buf, int len);
 };
 
-int Protocol::addProtocol(char* ndata, int len, int i, Protocol**a)
+int ProtocolList::addProtocol(char* ndata, int len, int i)
 {
     if (strstr(ndata, HexData) != NULL)
     {
@@ -53,37 +79,26 @@ int Protocol::addProtocol(char* ndata, int len, int i, Protocol**a)
     return 0;
 }
 
-int Protocol::sendAll(Protocol** a, int num)
+int ProtocolList::sendAll(int num)
 {
     char imput[20];
     cout << "Введите название протокола: ";
     cin.getline(imput, 20);
     if (num == 0)
         num++;
-    if (addProtocol(imput, strlen(imput), num, a)==-1)
+    if (addProtocol(imput, strlen(imput), num)==-1)
         return -1;
     return 0;
 }
 
-int Protocol::readData()
+int ProtocolList::readFromFile(fstream& file, int i)//чтение данных из файла
 {
     char ndata[20];
-    char fname[40];
-    int k = 100;//максимальный размер массива
     int s = 0;
     int n = 20;//максимальная длина строки
-    Protocol** a;
-    cout << "Введите название файла: ";
-    cin.getline(fname, 40);
-    fstream file;//поток из файла
-    file.open(fname, ios::in);//открытие файла
-    if (errorFileOpen(file) == -1)
-        return -1;
-    a = new Protocol* [k];
-    int i = 0;
     while (!file.eof())//пока не конец файла
     {
-        file>> ndata;
+        file >> ndata;
         if (file.fail()) // если предыдущее извлечение оказалось неудачным,
         {
             file.clear(); // то возвращаем cin в 'обычный' режим работы
@@ -109,12 +124,29 @@ int Protocol::readData()
             delete[] a;
             return -1;
         }
-        if (addProtocol(ndata, s,i,a)==-1)
+        if (addProtocol(ndata, s, i) == -1)
             return -1;
         i++;
     }
+    return 0;
+}
+
+int ProtocolList::readData()
+{
+    char fname[40];
+    int k = 100;//максимальный размер массива
+    cout << "Введите название файла: ";
+    cin.getline(fname, 40);
+    fstream file;//поток из файла
+    file.open(fname, ios::in);//открытие файла
+    if (errorFileOpen(file) == -1)
+        return -1;
+    a = new Protocol* [k];
+    int i = 0;
+    if (readFromFile(file, i) == -1)
+        return -1;
     file.close();//закрытие файла
-    if (sendAll(a, i) == -1)
+    if (sendAll(i) == -1)
         return -1;
     for (int j = 0; j < i; j++)
         delete[]a[j];
@@ -149,7 +181,7 @@ void BinaryProtocol::send(char* buf, int len)
     cout << endl;
 }
 
-int Protocol::errorFileOpen(fstream& file)
+int ProtocolList::errorFileOpen(fstream& file)
 {
     if (!file)//проверка на правильность открытия файла
     {
@@ -160,7 +192,7 @@ int Protocol::errorFileOpen(fstream& file)
         return 0;
 }
 
-int Protocol::errorInputData(fstream& file)
+int ProtocolList::errorInputData(fstream& file)
 {
     if (file.fail())
     {
@@ -174,8 +206,7 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int num;
-    Protocol pr;
+    ProtocolList pr;
     if (pr.readData() == -1)
         return -1;
     return 0;
